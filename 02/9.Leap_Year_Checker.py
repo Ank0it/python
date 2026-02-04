989424f0fdadd2702ec941b574ad06a8f3a5b043
Ankit Patel <131870832+Ank0it@users.noreply.github.com>
2026-02-04 17:15:20 +0530

    Fix leap year check logic in the Leap Year Checker

--- 02/9.Leap_Year_Checker.py
+++ 02/9.Leap_Year_Checker.py
@@ -2,7 +2,7 @@
 
 year=int(input("Enter the year :"))
 
-if year%4==0:
+if (year%400==0) or (year%4==0 and year%100!=0):
     print("You entered a leaf year")
 else:
     print("You enetered a non leaf year")    
